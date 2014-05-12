466b1e3087fed35848424df08b487ef3418b4a57
Camilo Payan <cpayan@fiu.edu>
2014-05-12 16:53:06 -0400

    Removing some Label cruft from plugin.php. Look, guys, I'll add labels back in later. OKAY

--- plugin.php
+++ plugin.php
@@ -3,42 +3,40 @@
 
 /* Define your defaults here */
 
-/*Now, get the settings of the user */
-include_once("settings.php");
 
 /*Do the things of our plugin */
 
-$tileTypes['fiu-circle'] = array( /* Defaults*/
+$tileTypes['fiucircle'] = array( /* Defaults*/
 	"group"=>0,
 	"x"=>0,
 	"y"=>0,
-	'width'=>2,
+	'width'=>1,
 	'height'=>1,
 	"background"=>$defaultBackgroundColor,
 	"url"=>"",
 	"labelText"=>"",
-	"labelColor"=>$defaultLabelColor,
-	"labelPosition"=>$defaultLabelPosition,
-	"classes"=>"",
+	"classes"=>""
 );
-function tile_fiu-circle($group,$x,$y,$width,$url,$labelText,$labelColor,$classes,$img,$imgAlt,$imgTitle){
+function tile_fiucircle($group,$x,$y,$width,$url,$labelText,$classes,$img,$imgAlt,$imgTitle){
 	global $scale, $spacing, $scaleSpacing, $groupSpacing;
 	$marginTop = $y*$scaleSpacing+getMarginTop($group);
 	$marginLeft = $x*$scaleSpacing+getMarginLeft($group);
 	$tileWidth = $width*$scaleSpacing-$spacing;
 	$tileHeight = $width*$scaleSpacing-$spacing;
 	?>
-  	<a <?php echo makeLink($url);?> class="tile fiu-circle group<?php echo $group?> <?php echo $classes?>" style="
-    margin-top:<?php echo $marginTop?>px; margin-left:<?php echo $marginLeft?>px;
-	width:<?php echo $tileWidth?>px; height:<?php echo $tileHeight?>px;
+  	<a <?php echo makeLink($url);?> class="tile fiu-circle group<?php echo $group; ?> <?php echo $classes; ?>" style="
+    margin-top:<?php echo $marginTop; ?>px; margin-left:<?php echo $marginLeft; ?>px;
+	width:<?php echo $tileWidth; ?>px; height:<?php echo $tileHeight; ?>px;
 	background:<?php echo $background;?>;" <?php posVal($marginTop,$marginLeft,$tileWidth);?>> 
     
 		  <img src="<?php echo $img; ?>" alt="<?php echo $imgAlt; ?>" title="<?php echo $imgTitle; ?>" /> 
     
     <?php 
+	/*
 	if($labelText!=""){
 		echo "<div class='tileLabelWrapper'><div class='tileLabel'>".$labelText."</div></div>";
 	}
+	 */
 	?> 
     </a>
     <?php
