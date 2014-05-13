d00643553ed2e09feb4ae05c7bdca967d8470caf
Camilo Payan <cpayan@fiu.edu>
2014-05-13 10:55:08 -0400

    Reorganizing for readability. Also removed fiu-circle class from parent anchor

--- plugin.php
+++ plugin.php
@@ -23,12 +23,22 @@ function tile_fiucircle($group,$x,$y,$width,$url,$labelText,$classes,$img,$imgAl
 	$tileWidth = $width*$scaleSpacing-$spacing;
 	$tileHeight = $width*$scaleSpacing-$spacing;
 	?>
-  	<a <?php echo makeLink($url);?> class="tile fiu-circle group<?php echo $group; ?> <?php echo $classes; ?>" style="
-    margin-top:<?php echo $marginTop; ?>px; margin-left:<?php echo $marginLeft; ?>px;
-	width:<?php echo $tileWidth; ?>px; height:<?php echo $tileHeight; ?>px;
-	" <?php posVal($marginTop,$marginLeft,$tileWidth);?>> 
-<div class="fiu-circle" style="background-image:url('<?php echo $img; ?>');width: 100%; height: 100%;"/>
-    
+	<a <?php echo makeLink($url);?> 
+		class="tile group<?php echo $group; ?> <?php echo $classes; ?>" 
+		style="
+			margin-top:<?php echo $marginTop; ?>px; 
+			margin-left:<?php echo $marginLeft; ?>px;
+			width:<?php echo $tileWidth; ?>px; 
+			height:<?php echo $tileHeight; ?>px;
+		" 
+		<?php posVal($marginTop,$marginLeft,$tileWidth);?>> 
+		<div 
+			class="fiu-circle" 
+			style="
+				background-image:url('<?php echo $img; ?>');
+				width: 100%; height: 100%;
+		"/>
+	</a>
     
     <?php 
 	/*
@@ -37,7 +47,6 @@ function tile_fiucircle($group,$x,$y,$width,$url,$labelText,$classes,$img,$imgAl
 	}
 	 */
 	?> 
-    </a>
     <?php
 }
 ?>
